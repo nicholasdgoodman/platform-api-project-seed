59659376af6684ded5c1ad37d2d1021d1a019916
connormccafferty <connor.mccafferty@openfin.co>
2021-06-03 13:32:12 -0400

    Simplify, remove props etc.

--- js/preload-example-script.js
+++ js/preload-example-script.js
@@ -1,14 +1,12 @@
 console.log("preload script loaded...");
 
-async function init() {
+function init() {
     console.log("Init called. Setting window title");
     const win = fin.Window.getCurrentSync();
-    console.log(window);
     win.executeJavaScript('document.title ="Some Friendly Name"');
-    
-    win.once('maximized', async () => {
-        await win.updateOptions({ opacity: 1 });
-    });
+
+    // update opacity after maximized event to avoid white flash
+    win.once('maximized', async () => await win.updateOptions({ opacity: 1 }));
 }
 
 init();
